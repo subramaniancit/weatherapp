9e75489fc1fb429dc2d4234520c3dee33ecac4b6
subramaniancit <37737789+subramaniancit@users.noreply.github.com>
2018-11-25 10:20:57 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+to install the modules use either :
+
+npm install OR 
+yarn install
+
+To run the app :
+
+npm start OR 
+yarn start
---
+++ src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Titles = () => (
+	<div>
+		<h1 className="title-container__title">Weather Man</h1>
+
+	</div>
+);
+
+export default Titles;
